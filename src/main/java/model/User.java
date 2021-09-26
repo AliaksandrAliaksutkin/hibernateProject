8ffd5aa91d4a66da7778ceda1552f3332d08f5fd
Aliaksutkin_Aliaksandr <aleksutkinaliaksandr@gmail.com>
2021-09-26 21:02:58 +0300

    add class user

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,14 +1,30 @@
 package model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+@Entity
 public class User {
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private int id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "age")
     private int age;
+
+    public User(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
 }
